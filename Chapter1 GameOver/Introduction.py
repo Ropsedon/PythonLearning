505654bea80bc6707a1bea80beb36a6c53ddfd60
Alexander <glukhov.bank@gmail.com>
2020-10-27 22:18:12 +0200

    Added chapter 1

---
+++ Chapter1 GameOver/Introduction.py
@@ -0,0 +1,11 @@
+# Chapter 1 Introduction
+
+print("Game Over")
+input("\n\nPress any key for the exit")
+
+# Tasks 1
+print("Alexander Hlukhov")
+
+# Task 2
+print("Don't give up")
+input("\n\nPress any key for the exit")
